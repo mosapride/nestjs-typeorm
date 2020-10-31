5894cf969d4887761cc583138fd8686e5ccd3fbb
mosapride <mosapride01@gmail.com>
2020-11-01 06:23:52 +0900

    add: export service

--- src/endpoint/user/user.module.ts
+++ src/endpoint/user/user.module.ts
@@ -6,6 +6,7 @@ import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
+  exports : [UserService],
   providers: [UserService],
   controllers: [UserController],
 })
